75ab0576613c47e3ebf4965d1b60160591c00e42
markpfaff <markdpfaff@gmail.com>
2017-05-29 13:46:55 -0700

    derouting initial commit

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+
+include 'Views/templates/header.php';
+
+if($usertype=="admin"){
+    include 'Views/Admin/dashboard.php';
+}else{
+    include 'Views/Faculty/dashboard.php';    
+}
+
+include 'Views/templates/footer.php';
